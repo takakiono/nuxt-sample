b8ca0d2f37249937af127b5a0afaa8b597085da1
takakiono <takaki.ono@icloud.com>
2022-12-04 14:26:13 +0900

    todoの追加

---
+++ nuxt-sample/pages/fun.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Hellow Vue!!</h1>
+        <NuxtLink to="/">Home page</NuxtLink>
+    </div>
+</template>
---
+++ nuxt-sample/pages/todo_search.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <h1>TODO</h1>
+
+        <div class="form">
+            <form @submit.prevent="todoAdd">
+                <label>やること</label>
+                <input v-model="todo">
+                <button type="submit">Add</button>
+            </form>
+        </div>
+
+        <!-- todo一覧表示 -->
+        <ul>
+            <li type="circle" v-for="task in tasks" v-bind:key=task>{{ task.name }}, {{ task.limit}}</li>
+        </ul>
+
+        <NuxtLink to="/">Home page</NuxtLink>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            tasks: [
+                {
+                    name:'task01',
+                    limit:'2022/10/14',
+                    completeFlag:false,
+                },
+                {
+                    name:'task01',
+                    limit:'2022/10/14',
+                    completeFlag:false,
+                }
+            ],
+            todo:'',
+        }
+    },
+    methods:{
+        todoAdd() {
+            this.tasks.push({
+                name:this.todo,
+                limit:'2022/10/14',
+                completeFlag:false,
+            });
+            this.todo = '';
+        }
+    },
+}
+</script>
